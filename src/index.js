68767d1311380aeaa3a8c854346698ab815fce85
Pablo Spector <40707996+PabloElectricista@users.noreply.github.com>
2022-09-12 05:00:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import { Provider } from 'react-redux';
 import { Auth0Provider } from '@auth0/auth0-react'
 import "bootswatch/dist/quartz/bootstrap.min.css";
 import axios from 'axios';
-import {
-    REACT_APP_AUTH0_DOMAIN,
-    REACT_APP_AUTH0_CLIENT_ID
-} from './config'
+// import {
+//     REACT_APP_AUTH0_DOMAIN,
+//     REACT_APP_AUTH0_CLIENT_ID
+// } from './config'
 axios.defaults.baseURL = process.env.REACT_APP_API || "https://pf-serve.herokuapp.com/api";
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Auth0Provider
-            domain={REACT_APP_AUTH0_DOMAIN}
-            clientId={REACT_APP_AUTH0_CLIENT_ID}
+            domain={process.env.REACT_APP_AUTH0_DOMAIN}
+            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
             redirectUri={window.location.origin}
         >
             <Provider store={store}>
