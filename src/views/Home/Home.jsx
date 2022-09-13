2bafae209da9cd2f032814ab87db186d63636d84
Pablo Spector <pablo_spector@hotmail.com>
2022-09-13 01:18:21 -0300

    filter component

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Cards from '../../components/Card/Cards'
 import Pagination from '../../components/Pagination/Pagination';
+import Filterscomponent from '../../components/Filters/Filterscomponent'
 
 function Home() {
 
     return <div className="container">
-        <Cards />
+        <div className="d-flex justify-content-between m-3">
+            <Filterscomponent />
+            <Cards />
+        </div>
         <Pagination />
     </div>
 }
