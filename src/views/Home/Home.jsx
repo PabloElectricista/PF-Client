055663d0dd53ebcc43e9870798bc56a353979abe
Pablo Spector <pablo_spector@hotmail.com>
2022-09-14 18:45:36 -0300

    ver-productos

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,29 +1,32 @@
 import { useEffect, useReducer, useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import Product from "../../components/Products/Product";
-import data from "../../data";
+import Products from "../../components/Products/Products";
+import Pagination from "../../components/Pagination/Pagination";
 
 function Home() {
-  return (
-    <>
-      <h1>Productos Destacados</h1>
-      <div className="products">
+    return (
+        <>
+            <h1>Productos Destacados</h1>
+            <div className="products">
+                <Products />
+            </div>
+            <Pagination />
+            {/* <div className="products">
         {/* {loading ? (
           <div>Loading...</div>
         ) : error ? (
           <div>{error}</div>
           ) : ( */}
-        <Row>
+            {/* <Row>
           {data.products.map((product) => (
             <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
-              <Product product={product}></Product>
+              
             </Col>
           ))}
-        </Row>
-        )
-      </div>
-    </>
-  );
+        </Row> */}
+
+        </>
+    );
 }
 
 export default Home;
