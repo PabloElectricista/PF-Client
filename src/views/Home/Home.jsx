7c86424a466c695669c1791c01c52db170c116b9
Pablo Spector <pablo_spector@hotmail.com>
2022-09-14 21:34:10 -0300

    arreglo bugs del merge por los cambios

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useReducer, useState } from "react";
-import { Row, Col } from "react-bootstrap";
+// import { useEffect, useReducer, useState } from "react";
+// import { Row, Col } from "react-bootstrap";
 import Products from "../../components/Products/Products";
 import Pagination from "../../components/Pagination/Pagination";
 import Cards from '../../components/Card/Cards'
-import Pagination from '../../components/Pagination/Pagination';
-import Filterscomponent from '../../components/Filters/Filterscomponent'
-import Ordercomponent from '../../components/Order/Ordercomponent';
+// import Filterscomponent from '../../components/Filters/Filterscomponent'
+// import Ordercomponent from '../../components/Order/Ordercomponent';
 
 function Home() {
     return (
@@ -15,30 +14,8 @@ function Home() {
                 <Products />
             </div>
             <Pagination />
-            {/* <div className="products">
-        {/* {loading ? (
-          <div>Loading...</div>
-        ) : error ? (
-          <div>{error}</div>
-          ) : ( */}
-            {/* <Row>
-          {data.products.map((product) => (
-            <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
-              
-            </Col>
-          ))}
-        </Row> */}
         </>
     );
-
-    return <div className="container">
-            <Ordercomponent />
-        <div className="d-flex justify-content-between m-3">
-            <Filterscomponent />
-            <Cards />
-        </div>
-        <Pagination />
-    </div>
 }
 
 export default Home;
