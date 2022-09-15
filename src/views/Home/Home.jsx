486e92fbfea8d8c20b64480ff98d4923c832a54b
Pablo Spector <pablo_spector@hotmail.com>
2022-09-15 01:00:18 -0300

    filter component remake

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,18 +1,25 @@
 // import { useEffect, useReducer, useState } from "react";
-// import { Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Products from "../../components/Products/Products";
 import Pagination from "../../components/Pagination/Pagination";
+import Filters from '../../components/Filters/Filters'
+import Carousel from "../Carousel/Carousel"
 // import Cards from '../../components/Card/Cards'
-// import Filterscomponent from '../../components/Filters/Filterscomponent'
 // import Ordercomponent from '../../components/Order/Ordercomponent';
 
 function Home() {
     return (
         <>
-            <h1>Productos Destacados</h1>
-            <div className="products">
-                <Products />
-            </div>
+            <Carousel />
+
+            <Row className="d-flex justify-content-start">
+                <Col  xs={3}>
+                    <Filters />
+                </Col>
+                <Col xs={9}>
+                    <Products />
+                </Col>
+            </Row>
             <Pagination />
         </>
     );
