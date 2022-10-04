2d417e6cf5cc33fe905a872fd844298cb71ae36b
IvanRomano <ivan.ac.romano@gmail.com>
2022-10-04 16:42:20 -0300

    Carrucel andando ahora siiiii

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,56 +1,43 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { Container, Carousel, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import './Carousel.css'
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getProds } from "../../redux/actions/products";
+import { useSelector } from "react-redux";
 
 function Slider() {
 
   const products = useSelector((state) => state.products.products);
-
-
   const [selectedProducts, setSelectedProducts] = useState([])
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
     let carousel = JSON.parse(localStorage.getItem('carousel'))
-    if (carousel === null || carousel.length === 0) {
-      getProducts()
-    } else {
+    if (carousel !== null ) {
       setSelectedProducts(carousel)
     }
   }, 
   [])
 
+  useEffect(() => {
+    if (products && products.length > 0) {
+      let arrProd = products.filter((data, idx) =>
+        idx === 2 ||
+        idx === 4 ||
+        idx === 6
+      )
+      localStorage.setItem('carousel', JSON.stringify(arrProd))
+      setSelectedProducts(arrProd); 
+    }
+  }, 
+  [products])
 
   function onClick(e) {
-    localStorage.setItem("page", 0)
-    dispatch(getProds())
-    setTimeout(() => {
-      navigate(`/product/${e.target.id}`)
-    }, 1000);
+    navigate(`/product/${e.target.id}`)
   }
 
-
-  const getProducts = () => {
-
-    if (products && products.length > 0) {
-
-      let arrProd = products.filter((data) =>
-        data._id === '633b75c675ad20ded98d7e41'
-        || data._id === '633b75c675ad20ded98d7e44'
-        || data._id === '633b75c675ad20ded98d7e45'
-      )
-      setSelectedProducts(arrProd);
-      localStorage.setItem('carousel', JSON.stringify(arrProd))
-    }
-  }
   
-
+  
   return (
     <Container className="products-container">
       <h2 className="products-container-heading">Productos destacados</h2>
